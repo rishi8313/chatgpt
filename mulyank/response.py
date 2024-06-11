ced7caa9846576bbe0ea2dac6733c1158df35e1c
Ashish <ashishwebsite13@gmail.com>
2024-06-11 23:49:27 +0530

    Added more questions

--- mulyank/response.py
+++ mulyank/response.py
@@ -42,6 +42,13 @@ Current Debt score is {I}. One should go in {K}
     input_variables=["I","K"],
 output_parser = None)
 
+debt_allocation_ot = PromptTemplate(
+    template="""
+Invest {L}% of your Debt money in Long term Debt and {M}% of your debt money in Short term Debt.
+""",
+    input_variables=["L","M"],
+output_parser = None)
+
 large_cap_allocation_ot = PromptTemplate(
     template="""
 At current juncture : Allocation in Large cap should be {Q} %.
@@ -65,12 +72,23 @@ input_variables=["R","S"],
 output_parser=None
 )
 
+equity_decision_out = PromptTemplate(
+    template="""
+Broader inference for Limited action advisor is ( Looking for Entry & Exit ) to {F}.
+Broader inference for Minimalistic action advisor is ( Looking for Entry but will never Sell ) to {H}
+""",
+input_variables=["F","H"],
+output_parser=None
+)
+
 OUTPUT_TEMPLATES = {
     "market_scenario": [market_scenario_ot, None],
     "capital_allocation": [capital_allocation_ot, None],
     "investment_advice" : [investment_advice_ot, None],
     "debt_market_view" : [debt_market_view_ot, None],
+    "debt_allocation" : [debt_allocation_ot, None],
     "large_cap_allocation":[large_cap_allocation_ot,None],
     "small_mid_cap_allocation_current":[sm_cap_alloc_cur_ot,"add"],
-    "small_mid_cap_allocation_past":[sm_cap_alloc_cur_past,None]
+    "small_mid_cap_allocation_past":[sm_cap_alloc_cur_past,None],
+    "equity_decision":[equity_decision_out,None]
 }
